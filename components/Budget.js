607bfaf84455be675207da98170d816059b7cd19
Nicholas Pipitone <npip99@gmail.com>
2020-01-06 16:42:17 -0500

    Pie Chart

--- components/Budget.js
+++ components/Budget.js
@@ -1,9 +1,28 @@
 import React from "react";
 import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { PieChart } from 'react-native-chart-kit';
 
 export default function Budget(props) {
+  const pieChartData = [
+    {
+      name: 'Education',
+      value: 315000000,
+      color: '#f00'
+    },
+    {
+      name: 'Infrastructure',
+      value: 35000000,
+      color: '#0f0'
+    },
+    {
+      name: 'Transportation',
+      value: 90000000,
+      color: '#00f'
+    },
+  ];
   return (
     <View>
+      <PieChart data={pieChartData}/>
       <Text>THIS IS THE GOVERNMENT BUDGET</Text>
     </View>
   );
