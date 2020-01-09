79edde3ae34a4f36b0765d3b240668e9268346e4
Nicholas Pipitone <npip99@gmail.com>
2020-01-09 23:10:23 +0000

    new lines

--- api/eventsList.js
+++ api/eventsList.js
@@ -5,7 +5,9 @@ var events = [
     date: "January 9th, 2020",
     body: `
     Approximately 20-25% of cats in the US are declawed. However, there has been recent outcry due to the perception that such declawings are both painful and unnatural. It's currently illegal in Australia, and most of the EU. However, it remains legal throughout the US except for select municipalities such as Hollywood, California. Around 50-60% of Americans responded "yes" when asked if they believed declawing was an acceptable practice.
+
     Veternarians will in cases support declawing in situations where a family member is immuno-comprimised and could be at risk of infection or sickness if cut. Veternarians in many cases support declawing as a last-resort, when the only other option is abandoning the cat. Overall, there is increasing debate and outcry for the routine use of cat declawing for reasons such as saving furniture.
+
     Florida Senator Lauren of Broward County has filed a bill outlawing cat declawing for any reason other than therapeutic purposes. The penalty stands at $1,000 per infraction. Many would consider this a win for cat owners and cat protectants. However, a case is to be made where this law is too invasive into the personal desires of a cat owner. For example, declawing for the protection of an immuno-comprimised family member is outlawed under this bill, despite overall consensus that such a reason is justifiable. This bill can be perceived as too overbearing as it outlaws it for any human-related need, while the outcry is focused on declawing for unjustifiable reasons such as not having to file a cats nails anymore.
     `,
     source: "https://www.flsenate.gov/Session/Bill/2020/48",
